2c0ab0ac8da38d9682a14b73ce743ef182912ada
karlmcphee <32080714+karlmcphee@users.noreply.github.com>
2024-10-11 10:06:06 -0400

    s

--- app/src/main/java/com/example/photosearcher/MainActivity.kt
+++ app/src/main/java/com/example/photosearcher/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.example.photosearcher
 
-import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
+import android.widget.TextView.OnEditorActionListener
 import androidx.activity.ComponentActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.example.photosearcher.ui.theme.PhotoSearcherTheme
-import com.squareup.picasso.BuildConfig
 import retrofit2.Call
 
 
@@ -34,10 +29,6 @@ class MainActivity : ComponentActivity() {
         recyclerView.layoutManager = staggeredGridLayoutManager
         val customAdapter = Adapter(pictureList)
         recyclerView.adapter = customAdapter
-
-        val applicationInfo = applicationContext.packageManager.getApplicationInfo(
-            applicationContext.packageName, PackageManager.GET_META_DATA
-        )
         try {
             val applicationInfo = applicationContext.packageManager.getApplicationInfo(
                 applicationContext.packageName, PackageManager.GET_META_DATA
@@ -52,8 +43,7 @@ class MainActivity : ComponentActivity() {
         searchButton.setOnClickListener {
             var query = searchText.getText().toString()
             if(query != "") {
-                searchText.setText("")
-                RetrofitInstance.api.getPhotos(1, 10, key, query) //The number of images be set higher
+                RetrofitInstance.api.getPhotos(1, 25, key, query) //The number of images can be changed here
                     .enqueue(object : retrofit2.Callback<PhotoModel> {
                         override fun onResponse(
                             call: Call<PhotoModel>,
@@ -67,10 +57,7 @@ class MainActivity : ComponentActivity() {
                                     pictureList.add(picture.urls.small)
                                 }
                                 customAdapter.updateData(pictureList)
-                                //Log.d("photos", photos.toString())
-                                // Handle the photos, e.g., display them in a RecyclerView
                             } else {
-                                // Handle the error
                                 println("Error: ${response.code()} - ${response.message()}")
                                 Log.d("error", "API access/conversion error")
                             }
@@ -81,5 +68,13 @@ class MainActivity : ComponentActivity() {
             })
         }
         }
+        searchText.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+            Log.d("first", "first")
+            if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+              Log.d("second", "second")
+                searchButton.performClick();
+            }
+            false
+        })
     }
 }
